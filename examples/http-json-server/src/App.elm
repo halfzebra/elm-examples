8b62a912fcec79acd1b6e1459a0285713613caae
William H.P. Nielsen <whpn@mailbox.org>
2017-06-03 20:23:24 +0200

    feat: Add POST to http-json (#6)

--- examples/http-json-server/src/App.elm
+++ examples/http-json-server/src/App.elm
@@ -1,10 +1,10 @@
 module App exposing (..)
 
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (src)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, input)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode exposing (Decoder)
+import Json.Encode
 
 
 ---- MODEL ----
@@ -12,12 +12,13 @@ import Json.Decode exposing (Decoder)
 
 type alias Model =
     { data : Maybe Data
+    , inputValue : String
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { data = Nothing }, Cmd.none )
+    ( { data = Nothing, inputValue = "" }, Cmd.none )
 
 
 type alias Data =
@@ -32,10 +33,16 @@ decoder : Decoder Data
 decoder =
     Json.Decode.map Data (Json.Decode.field "content" Json.Decode.string)
 
+encoder inputValue = 
+    Http.jsonBody (Json.Encode.object [ ( "content", Json.Encode.string inputValue ) ])
 
+        
 type Msg
     = Send
     | Response (Result Http.Error Data)
+    | InputUpdate String
+    | SendData
+    | SendDataResponse (Result Http.Error Data) -- actually not the data
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -45,6 +52,21 @@ update msg model =
             ( model, Http.send Response (Http.get "http://localhost:3004/data" decoder) )
 
         Response res ->
+            case res of
+                Ok data ->
+                    ( { model | data = Just data }, Cmd.none )
+
+                Err err ->
+                    ( model, Cmd.none )
+
+        InputUpdate value ->
+            ( { model | inputValue = value }, Cmd.none )
+
+        SendData ->
+            ( model
+            , Http.send Response (Http.post "http://localhost:3004/data" ( encoder model.inputValue ) decoder) )
+
+        SendDataResponse res ->
             case res of
                 Ok data ->
                     ( { model | data = Just data }, Cmd.none )
@@ -62,6 +84,8 @@ view model =
     div []
         [ div [] [ text (toString model.data) ]
         , button [ onClick Send ] [ text "Get the data" ]
+        , input [ onInput InputUpdate ] []
+        , button [ onClick SendData ] [ text "Send data to server" ]
         ]
 
 
